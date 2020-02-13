7586afc555b43dcc9e77a8477a25383948eebf10
303106917 <303106917@qq.com>
2020-02-13 16:15:11 +0800

    新传入

---
+++ yyq456/Form1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace yyq456
+{
+    public partial class Form1 : Form
+    {
+        /// <summary>
+        /// abc
+        /// </summary>
+        public const int abc = 100;
+        public Form1()
+        {
+            InitializeComponent();
+            
+            int a = 0;
+            int x = 0;
+
+        }
+    }
+}
---
+++ yyq456/Form3.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace yyq456
+{
+    public partial class Form3 : Form
+    {
+        public string FristName {get; set;}
+        public int y = 0;
+        public int x = 0;
+        public Form3()
+        {
+            InitializeComponent();
+        }
+        public double tr = 0.0f;
+
+
+        int fasfa = 0;
+    }
+}
